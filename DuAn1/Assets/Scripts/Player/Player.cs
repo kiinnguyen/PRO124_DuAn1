e39910fcb580a65fbd53028e5873b8dfaf494962
Kiin Nguyễn <nguyenvantugamingchannel@gmail.com>
2024-07-09 11:09:02 +0700

    player

--- DuAn1/Assets/Scripts/Player/Player.cs
+++ DuAn1/Assets/Scripts/Player/Player.cs
@@ -137,6 +137,12 @@ public class Player : MonoBehaviour
 
     private bool isAction() => isActing;
 
+    private bool isPause()
+    {
+        // get bool value in systems script
+        return true;
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision detected with " + collision.gameObject.name);
