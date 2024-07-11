f8fd8242b41df7b5b31af721df2adcdb46909d00
Kiin Nguyễn <nguyenvantugamingchannel@gmail.com>
2024-07-11 22:38:10 +0700

    player

--- DuAn1/Assets/Scripts/Player/PlayerManager.cs
+++ DuAn1/Assets/Scripts/Player/PlayerManager.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : Singleton<PlayerManager>
+public class PlayerManager : MonoBehaviour
 {
     int health { get; set; }
     int gold { get; set; }
 
-    public PlayerManager(int health, int gold)
+    int food { get; set; }
+
+    int water { get; set; }
+
+    int damage { get; set; }
+
+    public PlayerManager() { }
+
+    public PlayerManager(int health, int gold, int food, int water)
     {
         this.health = health;
         this.gold = gold;
+        this.food = food;
+        this.water = water;
     }
 
-    public void SetPlayerData(int healthInput, int goldInput)
+    public void SetPlayerData(int healthInput, int goldInput, int foodInput, int waterInput)
     {
         health = healthInput;
         gold = goldInput;
+        food = foodInput;
+        water = waterInput;
     }
 
     public int GetHealth()
@@ -24,8 +36,33 @@ public class PlayerManager : Singleton<PlayerManager>
         return health;
     }
 
+    public void SetHealth(int healthInput)
+    {
+        health = healthInput;
+    }
+
     public int GetGold()
     {
         return gold;
+    }
+
+    public void SetGold(int goldInput)
+    {
+        gold = goldInput;
+    }
+
+    public int GetFood()
+    {
+        return food;
+    }
+
+    public int GetWater()
+    {
+        return water;
+    }
+
+    public int GetDamage()
+    {
+        return damage;
     }
 }
