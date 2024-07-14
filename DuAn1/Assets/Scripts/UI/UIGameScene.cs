5576410adf6770107cfbccb69445d3856be02a2b
Kiin Nguyễn <nguyenvantugamingchannel@gmail.com>
2024-07-14 23:33:19 +0700

    UI

--- DuAn1/Assets/Scripts/UI/UIGameScene.cs
+++ DuAn1/Assets/Scripts/UI/UIGameScene.cs
@@ -8,6 +8,29 @@ public class UIGameScene : MonoBehaviour
     [Header("Speaker")]
     [SerializeField] AudioSource musicSpeaker;
 
+    [Header("UI Container")]
+    [SerializeField] GameObject inventoryBar;
+    bool isInventoryBarActive;
+    bool isOpenningInventory;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!isOpenningInventory)
+            StartCoroutine(OpenInventory());
+        }
+    }
+
+    IEnumerator OpenInventory()
+    {
+        isOpenningInventory = true;
+        isInventoryBarActive = inventoryBar.active ? false : true;
+        inventoryBar.SetActive(isInventoryBarActive);
+        yield return new WaitForSeconds(1f);
+        isOpenningInventory = false;
+    }
+
 
     public void ReturnMenuScene()
     {
