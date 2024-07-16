ab4cae65f284ee92afed14f109c8219e7832528a
HoangLam712005 <zzlam123@gmail.com>
2024-07-17 00:43:35 +0700

    save

--- DuAn1/Assets/lam/item/Item.cs
+++ DuAn1/Assets/lam/item/Item.cs
@@ -1,18 +1,28 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class item
     {
-        
+        public string itemName;
+        public Sprite itemIcon;
+        public ItemType itemType;
+        public int amount;
+
+        public virtual void Use(Player player)
+        {
+            // Logic để sử dụng item sẽ được override bởi các class con
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum ItemType
     {
-        
+        itemHealth,
+        itemMana
     }
+
+
 }
--- DuAn1/Assets/lam/item/itemHealth.cs
+++ DuAn1/Assets/lam/item/itemHealth.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class itemHealth : MonoBehaviour
+public class itemHealth : Item
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    
+        public int healAmount;
+
+        public override void Use(Player player)
+        {
+            player.Health(healAmount);
+        }
+    
 }
