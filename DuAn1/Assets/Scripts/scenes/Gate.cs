8e6812c2d32ecab1f928da66679c4b4152932735
HoangLam712005 <zzlam123@gmail.com>
2024-07-29 01:50:23 +0700

    Gate

---
+++ DuAn1/Assets/Scripts/scenes/Gate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public sceneManager transitionManager; 
+    public string targetSceneName = "Scene3"; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            transitionManager.TransitionToScene(targetSceneName);
+        }
+    }
+}
