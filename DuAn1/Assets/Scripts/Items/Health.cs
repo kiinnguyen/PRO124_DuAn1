63eefecf8f1bfd58a21ddd1d21819f91d1434412
Kiin Nguyễn <nguyenvantugamingchannel@gmail.com>
2024-08-08 07:37:39 +0700

    new cave

--- DuAn1/Assets/Scripts/Items/Health.cs
+++ DuAn1/Assets/Scripts/Items/Health.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-
-    public int maxHealth = 100; // Máu tối đa
+    public int maxHealth = 20;
     private int currentHealth;
 
     void Start()
     {
-        currentHealth = maxHealth; // Khởi tạo máu hiện tại
+        currentHealth = maxHealth;
     }
 
     public void TakeDamage(int damage)
@@ -25,16 +24,7 @@ public class Health : MonoBehaviour
 
     void Die()
     {
-        // Thêm hiệu ứng phá hủy nếu cần thiết, ví dụ như chơi âm thanh, animation v.v.
-        Destroy(gameObject); // Phá hủy game object
+        Destroy(gameObject);
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player") // Kiểm tra nếu va chạm với đạn
-        {
-            TakeDamage(10); // Gây sát thương 10
-        }
-
-    }
 }
