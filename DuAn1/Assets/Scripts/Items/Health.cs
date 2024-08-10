d7840d892838699bbc67c9f6b44aee824a9832d1
hoangphuc240 <dhphuc8704@gmail.com>
2024-08-10 20:29:26 +0700

    S.O.S

--- DuAn1/Assets/Scripts/Items/Health.cs
+++ DuAn1/Assets/Scripts/Items/Health.cs
@@ -7,11 +7,12 @@ public class Health : MonoBehaviour
     public int maxHealth = 20;
     private int currentHealth;
 
+    public float slowDuration = 2.0f; 
+    public float slowMultiplier = 0.5f; 
     void Start()
     {
         currentHealth = maxHealth;
     }
-
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
@@ -27,4 +28,25 @@ public class Health : MonoBehaviour
         Destroy(gameObject);
     }
 
+    //void OnCollisionEnter(Collision collision)
+    //{
+        //if (collision.gameObject.tag == "Player") 
+        //{
+            //StartCoroutine(SlowPlayer(collision.gameObject));
+        //}
+    //}
+
+    //private IEnumerator SlowPlayer(GameObject player)
+    //{
+        //Player playerMovement = player.GetComponent<Player>();
+
+        //if (playerMovement != null)
+        //{
+            //float originalSpeed = playerMovement.speedmove;
+            //playerMovement.speedmove *= slowMultiplier; 
+
+            //yield return new WaitForSeconds(slowDuration);
+            //playerMovement.speedmove = originalSpeed; 
+        //}
+    //}
 }
